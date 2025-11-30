02f2ae7da73a9a8ebda8c0f2821936466cb5ee8b
rolandoandres22 <116299740+rolandoandres22@users.noreply.github.com>
2025-11-29 22:40:31 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import logo from "../assets/logo.jpg";
+
 function Header() {
-  return <img class="logo" src="../public/logo.jpg" alt="logo" />;
+  return <img className="logo" src={logo} alt="logo" />;
 }
 
 export default Header;
